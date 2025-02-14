f5d95b9d9445c90f43477a483bf07a99d9b40134
totetmatt <matthieu.totet@gmail.com>
2025-02-14 12:38:42 +0100

    v0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ffcmdr',  # Replace with your package’s name
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
